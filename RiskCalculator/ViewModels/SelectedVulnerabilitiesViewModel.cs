8812798bc58f44185b95d4786fc3de09b6b545bb
egor.chasnovskii <egor.chasnovskii@gmail.com>
2020-05-02 19:14:07 +0300

    Added ability to remove vulnerability

--- RiskCalculator/ViewModels/SelectedVulnerabilitiesViewModel.cs
+++ RiskCalculator/ViewModels/SelectedVulnerabilitiesViewModel.cs
@@ -10,16 +10,40 @@ namespace RiskCalculator.ViewModels
 {
     class SelectedVulnerabilitiesViewModel : Screen
     {
+        private VulnerabilityModel _selectedVulnerability;
+
         public SelectedVulnerabilitiesViewModel(BindableCollection<VulnerabilityModel> vulnerabilities)
         {
             VulnerabilitiesList = vulnerabilities;
         }
 
-        public BindableCollection<VulnerabilityModel> VulnerabilitiesList { get; set; } 
+        public BindableCollection<VulnerabilityModel> VulnerabilitiesList { get; set; }
+
+        public VulnerabilityModel SelectedVulnerability
+        {
+            get => _selectedVulnerability;
+            set
+            {
+                if (value != null)
+                {
+                    _selectedVulnerability = value;
+                }
+
+                NotifyOfPropertyChange(() => SelectedVulnerability);
+            }
+        }
 
 
         public void Edit()
         {
+        }
+
+        public void Delete()
+        {
+            if(VulnerabilitiesList.Count > 0)
+            {
+                VulnerabilitiesList.Remove(SelectedVulnerability);
+            }
         }
 
         //protected override void OnActivate()
